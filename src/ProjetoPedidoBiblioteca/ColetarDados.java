82c6bacf5559a4217ce05b3b4337e430bb5717b1
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-03-21 19:12:27 -0300

    adicionando mais variaveis

---
+++ src/ProjetoPedidoBiblioteca/ColetarDados.java
@@ -0,0 +1,35 @@
+package ProjetoPedidoBiblioteca;
+
+import com.sun.org.apache.xpath.internal.objects.XString;
+
+public class ColetarDados {
+    private String nome;
+    private string telefone;
+    private String NomeLivro;
+
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public string getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(string telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getNomeLivro() {
+        return NomeLivro;
+    }
+
+    public void setNomeLivro(String nomeLivro) {
+        NomeLivro = nomeLivro;
+    }
+}
