da94930813244a52e67b900b9ba6a156fc3c7357
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-03-22 19:50:30 -0300

    finalização do projetinho

--- src/CalculadoraS/Main.java
+++ src/CalculadoraS/Main.java
@@ -7,13 +7,17 @@ public class Main {
      ColetarDados var = new ColetarDados();
      Scanner scan = new Scanner(System.in);
 
-     System.out.print("Digite o primeiro numero");
+     System.out.print("\nDigite o primeiro numero: ");
      double primeiroNum = scan.nextDouble();
      var.setNum1(primeiroNum);
 
-     System.out.print("Digite os segundo numero: ");
+     System.out.print("\nDigite os segundo numero: ");
      double segundoNum = scan.nextDouble();
      var.setNum2(segundoNum);
+
+
+     double resultado = FunctionAction.function(var);
+     System.out.print("\nO resultado dessa soma deu: "+resultado);
 
 
 
