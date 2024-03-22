2e5db8ef12419202ec38aa7402b3a873e819116c
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-03-22 19:46:32 -0300

    Criação dos scan

--- src/CalculadoraS/Main.java
+++ src/CalculadoraS/Main.java
@@ -1,7 +1,21 @@
 package CalculadoraS;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
+     ColetarDados var = new ColetarDados();
+     Scanner scan = new Scanner(System.in);
+
+     System.out.print("Digite o primeiro numero");
+     double primeiroNum = scan.nextDouble();
+     var.setNum1(primeiroNum);
+
+     System.out.print("Digite os segundo numero: ");
+     double segundoNum = scan.nextDouble();
+     var.setNum2(segundoNum);
+
+
 
 
     }
