982f2c1aebe0dcbb45a67e60d5c78b0b9e919b52
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-03-25 20:35:39 -0300

    modifciação basica7

--- src/ProjetoPedidoBiblioteca/Main.java
+++ src/ProjetoPedidoBiblioteca/Main.java
@@ -23,7 +23,7 @@ public class Main {
         System.out.print("\n----- Ficha tecnica -----");
         System.out.print("\n Olá "+dados.getNome()+" tudo bem?");
         System.out.print("\n Seu Numero de telefone para contato é: "+dados.getTelefone());
-        System.out.print("\n você está levando o livro: "+dados.getNomeLivro());
+        System.out.print("\n Você está levando o livro: "+dados.getNomeLivro());
 
 
 
