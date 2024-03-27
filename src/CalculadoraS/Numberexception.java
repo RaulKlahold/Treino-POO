18de5de5baa413b0b3a647fe5a9ba937c2218015
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-03-26 22:01:30 -0300

    Adição da exception

--- src/CalculadoraS/Numberexception.java
+++ src/CalculadoraS/Numberexception.java
@@ -1,4 +1,11 @@
 package CalculadoraS;
 
-public class Numberexception {
+public class Numberexception extends Exception{
+    public Numberexception() {
+        super("Só é permitido apenas 1 pedido por vez!");
+    }
+
+    public Numberexception(String message) {
+        super(message);
+    }
 }
