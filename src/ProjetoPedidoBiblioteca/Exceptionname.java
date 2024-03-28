72be89167b87cd0ef024a697f1e29465b9dc813a
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-03-27 22:02:49 -0300

    criando exception

--- src/ProjetoPedidoBiblioteca/Exceptionname.java
+++ src/ProjetoPedidoBiblioteca/Exceptionname.java
@@ -1,4 +1,8 @@
 package ProjetoPedidoBiblioteca;
 
-public class Exceptionname {
+public class Exceptionname extends Exception {
+    public Exceptionname(){
+        super("O nome deve conter mais que 5 caracteres");
+    }
+
 }
