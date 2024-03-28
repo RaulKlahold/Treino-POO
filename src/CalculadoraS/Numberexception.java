1156f1bd1a9ef8a73f492fb5c780bdac393f064b
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-03-27 22:05:41 -0300

    Colocando ele na main para rodar certo

--- src/CalculadoraS/Numberexception.java
+++ src/CalculadoraS/Numberexception.java
@@ -2,7 +2,7 @@ package CalculadoraS;
 
 public class Numberexception extends Exception{
     public Numberexception() {
-        super("O numero tem que ser maior que 0");
+        super("O numero tem que ser maior que 3");
     }
 
 
