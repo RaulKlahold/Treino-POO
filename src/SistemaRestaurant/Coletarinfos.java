a2d3b807a029b5a95062335bb6bd5c1d17b4b694
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-04-02 22:53:07 -0300

    finalizando a primeira parte dos pedidos

--- src/SistemaRestaurant/Coletarinfos.java
+++ src/SistemaRestaurant/Coletarinfos.java
@@ -5,7 +5,30 @@ public class Coletarinfos {
     private String Cpf;
     private Integer pedido;
 
+    public Coletarinfos() {
+    }
 
+    public String getNome() {
+        return Nome;
+    }
 
+    public void setNome(String nome) {
+        Nome = nome;
+    }
 
+    public String getCpf() {
+        return Cpf;
+    }
+
+    public void setCpf(String cpf) {
+        Cpf = cpf;
+    }
+
+    public Integer getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Integer pedido) {
+        this.pedido = pedido;
+    }
 }
