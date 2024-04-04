69810a8ac1108812eccb99f0e757ee55c14d4026
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-04-03 21:33:17 -0300

    criação das variaveis locais de pedidos

--- src/SistemaRestaurant/ColetarPedidos.java
+++ src/SistemaRestaurant/ColetarPedidos.java
@@ -4,6 +4,9 @@ public class ColetarPedidos {
 
        public static String Pedido(Coletarinfos Coletarinfos){
 
+           String Nome = Coletarinfos.getNome();
+           String CPF = Coletarinfos.getCpf();
+           Integer NumeroPedido = Coletarinfos.getPedido();
 
 
 
