3923e2f147ede0ca68055e35784c27d21d30f91e
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-04-04 21:48:01 -0300

    objetos instanciados

--- src/SistemaRestaurant/Main.java
+++ src/SistemaRestaurant/Main.java
@@ -1,7 +1,12 @@
 package SistemaRestaurant;
 
+import java.util.Scanner;
+
 public class Main {
      public static void main(String[] args){
+       Coletarinfos infos = new Coletarinfos();
+       Scanner scan = new Scanner(System.in);
+
 
 
 
