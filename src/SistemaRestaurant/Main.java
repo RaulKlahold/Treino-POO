1a7b200932c1afe40bf14cb37abe8b1595ce8c09
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-04-10 22:53:35 -0300

    Terminado a Exception name

--- src/SistemaRestaurant/Main.java
+++ src/SistemaRestaurant/Main.java
@@ -3,13 +3,16 @@ package SistemaRestaurant;
 import java.util.Scanner;
 
 public class Main {
-     public static void main(String[] args) {
+     public static void main(String[] args) throws ExceptioName {
        Coletarinfos infos = new Coletarinfos();
        Scanner scan = new Scanner(System.in);
 
-       System.out.print("Digite seu nome: ");
-       String name = scan.nextLine();
-       infos.setNome(name);
+
+
+         System.out.print("Digite seu nome: ");
+         String name = scan.nextLine();
+         infos.setNome(name);
+
 
        System.out.print("Digite seu CPF: ");
        String Cpf = scan.nextLine();
@@ -21,16 +24,19 @@ public class Main {
 
        try {
          String ficha = ColetarPedidos.Pedido(infos);
-
-         System.out.print("\n------- Ficha do Pedido --------\n");
-         System.out.print("\n Olá," + infos.getNome() + "Seja bem vindo!");
-         System.out.print("\nUsuário do CPF: " + infos.getCpf());
-         System.out.print("\nSua pedido foi : \n" + ficha);
-         System.out.print("\nObrigado pela preferência! \n");
-
+         if(!(infos.getNome().length() < 3)) {
+           System.out.print("\n------- Ficha do Pedido --------\n");
+           System.out.print("\n Olá," + infos.getNome() + "Seja bem vindo!");
+           System.out.print("\nUsuário do CPF: " + infos.getCpf());
+           System.out.print("\nSua pedido foi : \n" + ficha);
+           System.out.print("\nObrigado pela preferência! \n");
+         }
+         else {
+           throw new ExceptioName();
+         }
        } catch (ExceptioNumeroPedido e){
          System.out.print(e.getMessage());
-       }
+       } catch ()
 
 
 
