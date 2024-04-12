864b8466852e9dbc2c1add00be9447107f2f904f
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-04-11 23:08:37 -0300

    Arrumando Exception cpf

--- src/SistemaRestaurant/CPFException.java
+++ src/SistemaRestaurant/CPFException.java
@@ -1,5 +1,6 @@
 package SistemaRestaurant;
 
-public class CPFException {
-
-}
+public class CPFException extends Exception {
+      public CPFException(){
+      super("O CPF deve conter 11 numeros!");
+}}
