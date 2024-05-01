46c81286a9ec563c684e5c44fe40e9ba4f7f6706
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-05-01 16:41:27 -0300

    coomitando no feriado

--- src/TreinoInterface/main.java
+++ src/TreinoInterface/main.java
@@ -2,7 +2,7 @@ package TreinoInterface;
 
 public class main {
     public static void mainClass(String[] args){
-        
+         System.out.println("Diga a raça de um cachorro");
 
         
         
