e2ee330b02b166bb57051d89aa4172e79f32ed06
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-05-01 16:46:06 -0300

    trabalhando a main

--- src/TreinoInterface/main.java
+++ src/TreinoInterface/main.java
@@ -1,8 +1,14 @@
 package TreinoInterface;
 
+import java.util.Scanner;
+
 public class main {
     public static void mainClass(String[] args){
+        Cachorro cachorro = new Cachorro();
+        Scanner scan =new Scanner(System.in);
+
          System.out.println("Diga a raça de um cachorro");
+         String cachorro1
 
         
         
