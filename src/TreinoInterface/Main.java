d34b2785cb5ba9ed6de92a735351a4cc4cbb51a3
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-05-02 21:07:20 -0300

    consegui etar uma exception sozinho!

--- src/TreinoInterface/Main.java
+++ src/TreinoInterface/Main.java
@@ -3,7 +3,7 @@ package TreinoInterface;
 import java.util.Scanner;
 
 public class Main {
-    public static void mainClass(String[] args){
+    public static void main(String[] args){
         Cachorro cachorro = new Cachorro();
         Scanner scan =new Scanner(System.in);
 
